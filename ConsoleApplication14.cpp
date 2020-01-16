1a10dde9417226a8bf702a032d5a706ec782035f
Anna Selezneva <55618935+ktiha@users.noreply.github.com>
2020-01-16 20:13:40 +0300

    Add files via upload

---
+++ ConsoleApplication14.cpp
@@ -0,0 +1,34 @@
+﻿
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int s, l1, r1, l2, r2, k;
+	int n = 0;
+	cin >> s >> l1 >> r1 >> l2 >> r2;
+	for (int i = l1; i <= r1; i++) {
+		for (int c = l2; c <= r2; c++) {
+			k = i + c;
+			if (k == s) {
+				n = 1;
+				cout << i << c;
+				break;
+
+			
+		}
+			
+		
+		}
+		if (k == s) {
+			
+			break;
+
+		}
+
+	}
+	if (n == 0) {
+		cout << -1;
+}
+}
+
---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,33 @@
+﻿// ConsoleApplication3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+	cout << a + b << " " << a - b << " " << a / b << " " << a * b<<endl;
+	double a1, b1;
+	cin >> a1 >> b1;
+	cout << a1 + b1 << " " << a1 - b1 << " " << a1 / b1 << " " << a1 * b1 << endl;
+	double a2, c, c1, c2, c3;
+	int b2;
+	cin >> a2 >> b2;
+	c = a2 + b2;
+	c1 = a2- b2;
+	c2 = a2 / b2;
+	c3 = a2 * b2;
+	cout << c << " " << c1 << " " << c2 << " " << c3 << endl;
+	double b3, cx, cx1, cx2, cx3;
+	int a3;
+	cin >> a3 >> b3;
+	cx = a3 + b3;
+	cx1 = a3 - b3;
+	cx2 = a3 / b3;
+	cx3 = a3 * b3;
+	cout << cx << " " << cx1 << " " << cx2 << " " << cx3 << endl;
+	return 0;
+}
+
+
