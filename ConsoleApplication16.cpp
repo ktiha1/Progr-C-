e37ee7733612feaa8a8e1008749faa632084975a
Anna Selezneva <55618935+ktiha@users.noreply.github.com>
2020-01-17 11:09:11 +0300

    Add files via upload

---
+++ ConsoleApplication16.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int fac(int n){
+    for (int i = 2; i <= n; i++) if (!(n%i)) return i;
+    return 1;
+}
+
+void factorization(int num){
+    for (int i = 2; i <= num;){
+        if (!(num%i)){}
+    }
+
+}
+
+int main (){
+    int num;
+
+    cin >> num;
+
+    factorization(num);
+
+    return 0;
+}
---
+++ ConsoleApplication21.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+int main(){
+    int array[10][8] = {};
+    int row, col, att = 5;
+    srand(time(NULL));
+
+    int* ptr = &array[0][0];
+    int num = rand() % (10 * 8 -1);
+    cout << num/8  << ' ' << num%8 << endl;
+    *(ptr + num) = 1;
+//          или
+//    *(&array[0][0] + rand() % (10 * 8 - 1)) = 1;
+
+    while (att){
+        cout << endl << "Осталось попыток: " << att << endl;
+        cin >> row >> col;
+        if ((row < 0) || (row > 9) || (col < 0) || (col > 7)) cout << "Вышли за рамки" << endl;
+        else if (array[row][col] == -1) cout << "Не корректный ввод" << endl;
+        else if (array[row][col] == 1){
+            cout << "Вы победили" << endl;
+            break;
+        }
+        else {
+            cout << "Не верный выбор" << endl;
+            array[row][col] = -1;
+            att--;
+        }
+    }
+    if (!att) cout << "Вы проиграли" << endl;
+    return 0;
+}
